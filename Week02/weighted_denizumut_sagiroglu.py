dc7587fa0d101a71b41b42a370e32be7253544b4
denizumut1 <codexdeniz@gmail.com>
2026-06-02 22:28:12 +0300

    Update weighted_denizumut_sagiroglu.py

--- Week02/weighted_denizumut_sagiroglu.py
+++ Week02/weighted_denizumut_sagiroglu.py
@@ -4,7 +4,6 @@ def weighted_srs(data, n, weights, with_replacement=False):
         return random.choices(data, weights=weights, k=n)
     pool_d, pool_w, sample = list(data), list(weights), []
     for _ in range(n):
-        i = random.choices(range(len(pool_d)), weights=pool_w, k=1) [0]
-        sample.append(pool_d.pop(i))
-        pool_w.pop(i)
+        i = random.choices(range(len(pool_d)), weights=pool_w, k=1)[0]
+        sample.append(pool_d.pop(i)); pool_w.pop(i)
     return sample
