be8ce5130a9ee1d4d2ff9a217866a4f45a2dcfaf
denizumut1 <codexdeniz@gmail.com>
2026-06-02 22:15:36 +0300

    Update weighted_denizumut_sagiroglu.py

--- Week02/weighted_denizumut_sagiroglu.py
+++ Week02/weighted_denizumut_sagiroglu.py
@@ -1,5 +1,5 @@
 import random
-def weighted_srs(data, n, weights, with_replacement):
+def weighted_srs(data, n, weights, *, with_replacement):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
     pool_d, pool_w, sample = list(data), list(weights), []
